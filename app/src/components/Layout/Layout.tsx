36eab54820661b9815fb59a5979182d49db12a79
Felix-Hu-DotDev <felix@dotcommerce.com.au>
2024-08-09 14:11:32 +1000

    feature: return to top

--- app/src/components/Layout/Layout.tsx
+++ app/src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@app/components/Header/Header";
 import Footer from "@app/components/Footer/Footer";
 import { useRef } from "react";
+import ReturnTop from "@app/components/ReturnTop";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -12,8 +13,9 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col md:flex-row">
       <Header />
-      <main className="w-full bg-[#fdf6e3]" ref={mainRef}>
+      <main className="w-full bg-[#fdf6e3] relative" ref={mainRef}>
         {children}
+        <ReturnTop />
         <Footer />
       </main>
     </div>
