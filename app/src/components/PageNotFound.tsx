7180ac4f03a77f64ff6174f04800348ba2acadda
Felix-Hu-DotDev <felix@dotcommerce.com.au>
2024-08-28 13:11:20 +1000

    chore: cleanup code

--- app/src/components/PageNotFound.tsx
+++ app/src/components/PageNotFound.tsx
@@ -7,8 +7,7 @@ const PageNotFound: React.FC = () => {
 
   return (
     <div className="w-[100%] py-12 px-8 lg:py-16 2xl:py-24 lg:px-16 h-[93dvh] flex flex-col items-center justify-center">
-      <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-x-[10rem] gap-y-[2.5rem] max-w-[1440px] mx-auto"></div>
-      <h1 className="mt-4 text-4xl lg:text-6xl font-bold text-[#002b36] mb-4">
+      <h1 className="mt-4 text-4xl lg:text-6xl font-bold text-[#002b36] mb-4 text-center">
         {title}
       </h1>
       <p className="text-primary md:px-10 lg:px-16 mb-8 w-full text-center">
