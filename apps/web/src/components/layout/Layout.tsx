1551359586f496c637586e8aaff766017ebb1be5
Icekraks <felix.hu847@gmail.com>
2026-05-12 14:23:52 +1000

    chore: analytics

--- apps/web/src/components/layout/Layout.tsx
+++ apps/web/src/components/layout/Layout.tsx
@@ -4,6 +4,7 @@ import { useRoot } from "@/providers/root-provider";
 import { Header } from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import ReturnTop from "@/components/ReturnTop";
+import { Analytics } from "@vercel/analytics/next";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -22,6 +23,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className="flex flex-col min-h-dvh">
+      <Analytics />
       <Header />
       <main className="w-full bg-[#fdf6e3] flex-1">
         {children}
